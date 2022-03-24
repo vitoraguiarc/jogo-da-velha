dac7ec0b976484b3052f43853ac3dc75c635643a
VITOR AGUIAR CUNHA <21276756@cfp127aluno.local>
2022-03-24 16:58:40 -0300

    telas feitas

---
+++ app/src/main/java/com/example/jogodavelha/SelecaoKilluaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jogodavelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelecaoKilluaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selecao_killua)
+    }
+}
---
+++ app/src/main/java/com/example/jogodavelha/VitoriaGonActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jogodavelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VitoriaGonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vitoria_gon)
+    }
+}
